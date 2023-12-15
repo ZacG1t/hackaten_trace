901993ddeebbaa50afb343c9e2fd62bf4023a93d
ZacG1t <teozhelun@gmail.com>
2023-12-15 17:48:16 +0800

    adding files

---
+++ app.py
@@ -0,0 +1,39 @@
+import streamlit as st 
+from PIL import Image 
+from io import BytesIO
+import requests
+import base64
+import numpy as np
+
+import backend
+
+st.title('Trace')
+
+style_names = ["Hosoda", "Hayao", "Shinkai", "Paprika"]
+style_name = st.sidebar.selectbox('Select Style', options=style_names)
+
+model = "pretrained_models/" + style_name + ".pth" 
+
+uploaded_image = st.sidebar.file_uploader("Upload image", type=["png", "jpg", "jpeg"])
+
+cols = st.columns((1,1))
+with cols[0]: 
+    input_image = st.empty() 
+with cols[1]: 
+    transformed_image = st.empty()
+
+if uploaded_image is not None: 
+    clicked = st.sidebar.button("Trace")
+    pil_image = Image.open(uploaded_image) 
+    image = base64.b64encode(uploaded_image.getvalue()).decode("utf-8")
+
+    input_image.image(uploaded_image)
+
+    if clicked: 
+        model = backend.load_models(model)
+        result = backend.transform(model, input=uploaded_image)
+
+        st.write("### Output image:")
+        st.image(result)
+        
+        
---
+++ backend.py
@@ -0,0 +1,93 @@
+import json
+from io import BytesIO
+import time
+import os
+import base64
+
+import numpy as np
+from PIL import Image
+
+import torch
+import torchvision.transforms as transforms
+from torch.autograd import Variable
+import torchvision.utils as vutils
+from Transformer import Transformer
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+def img_to_base64_str(img):
+    buffered = BytesIO()
+    #img.save(buffered, format="PNG")
+    buffered.seek(0)
+    img_byte = buffered.getvalue()
+    img_str = "data:image/png;base64," + base64.b64encode(img_byte).decode()
+    return img_str
+
+
+def load_models(model_path):
+    print("load model")
+    with torch.no_grad():
+        model = Transformer()
+        state = torch.load(model_path)
+        model.load_state_dict(state)
+        model.to(device)
+        model.eval()
+
+        return model
+
+
+gpu = -1
+
+mapping_id_to_style = {0: "Hosoda", 1: "Hayao", 2: "Shinkai", 3: "Paprika"}
+
+print(f"models loaded ...")
+
+
+def transform(model, input, load_size=450, gpu=-1):
+    models = {"Hosoda", "Hayao", "Shinkai", "Paprika"}
+    models_dict = {0: "Hosoda", 1: "Hayao", 2: "Shinkai", 3: "Paprika"}
+
+    if gpu > -1:
+        model.cuda()
+    else:
+        model.float()
+
+    input_image = Image.open(input).convert("RGB")
+    h, w = input_image.size
+
+    ratio = h * 1.0 / w
+
+    if ratio > 1:
+        h = load_size
+        w = int(h * 1.0 / ratio)
+    else:
+        w = load_size
+        h = int(w * ratio)
+
+    input_image = input_image.resize((h, w), Image.BICUBIC)
+    input_image = np.asarray(input_image)
+
+    input_image = input_image[:, :, [2, 1, 0]]
+    input_image = transforms.ToTensor()(input_image).unsqueeze(0)
+
+    input_image = -1 + 2 * input_image
+    if gpu > -1:
+        input_image = Variable(input_image).cuda()
+    else:
+        input_image = Variable(input_image).float()
+
+    t0 = time.time()
+    print("input shape", input_image.shape)
+    with torch.no_grad():
+        output_image = model(input_image)[0]
+    print(f"inference time took {time.time() - t0} s")
+
+    output_image = output_image[[2, 1, 0], :, :]
+    output_image = output_image.data.cpu().float() * 0.5 + 0.5
+
+    output_image = output_image.numpy()
+
+    output_image = np.uint8(output_image.transpose(1, 2, 0) * 255)
+    final_output_image = Image.fromarray(output_image)
+    
+    return final_output_image
